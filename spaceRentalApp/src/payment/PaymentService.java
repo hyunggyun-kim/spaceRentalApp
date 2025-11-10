5645bf700251598f8da1807e3bd5750c528d6ac0
som141 <163431679+som141@users.noreply.github.com>
2025-11-10 15:00:55 +0900

    dd

--- spaceRentalApp/src/payment/PaymentService.java
+++ spaceRentalApp/src/payment/PaymentService.java
@@ -26,34 +26,28 @@ public class PaymentService {
 
     }
 
-    public BigDecimal previewOnedayTotal(Reservation reservation) {
+    public BigDecimal previewOnedayTotal(Space space) {
         SecurityPaymentFactory spf = new SecurityPaymentFactory();
         ScalePaymentFactory cpf = new ScalePaymentFactory();
         SpaceTypePaymentFactory stpf = new SpaceTypePaymentFactory();
         BigDecimal result =new BigDecimal(0);
-        Space space = spaceRepository.findById(reservation.getSpaceId());
         result = stpf.get(space.getType()).apply(cpf.get(space.getScale()).apply(space.getSecurities().stream().map(e->spf.get(e).applyPayment(new BigDecimal(0))).reduce(BigDecimal::add).get()));
         return result;
-    }
-    public BigDecimal previewTotal(Reservation reservation){
-        int period=reservation.getTime().getPeriod();// 여기 time State메서드로 수정해줘야함.
-        BigDecimal base = previewOnedayTotal(reservation);
-        return base.multiply(new BigDecimal(period));
     }
 
 
     public BigDecimal OnedayTotal(Reservation reservation) {
         BigDecimal result =new BigDecimal(0);
-        BigDecimal base = previewOnedayTotal(reservation);
+        Space space=spaceRepository.findById(reservation.getSpaceId()).orElseThrow(()->new IllegalArgumentException("해당 장소가 없음.."));
+        BigDecimal base = previewOnedayTotal(space);
         User user = userRepository.findById(reservation.getUserId());
-        Space space = spaceRepository.findById(reservation.getSpaceId());
         UnitSpacePaymentFactory usp = new UnitSpacePaymentFactory();
         DiscountPolicyFactory dpf = new DiscountPolicyFactory();
         result =dpf.get(user.getUserGrade()).discount(base.add(space.getUnits().stream().map(e->usp.get(e).apply(new BigDecimal(0))).reduce(BigDecimal::add).get()));
 
         return result;
     }
-    public BigDecimal previewTotal(Reservation reservation){
+    public BigDecimal Total(Reservation reservation){
         int period=reservation.getTime().getPeriod();// 여기 time State메서드로 수정해줘야함.
         BigDecimal base =OnedayTotal(reservation);
         return base.multiply(new BigDecimal(period));
